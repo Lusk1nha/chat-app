49ff7bb3dcc76c796582a69a7733e6e1c5e8bce8
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2024-12-25 22:47:58 -0300

    🚧: Work in Progress: Creating frontend structures.

---
+++ app/src/app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function Dashboard() {
+  return (
+    <div className="min-w-full h-screen flex gap-2 p-4">
+      <Card className="w-80 h-full">
+        <CardContent>Teste</CardContent>
+      </Card>
+    </div>
+  );
+}
